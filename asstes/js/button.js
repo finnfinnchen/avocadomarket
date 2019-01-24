14f37e0630dc0437a78efec0a5fa88e251dc6082
Karl Rong <sjturjk@icloud.com>
2019-01-25 03:53:12 +0800

    Another syntax of javascript

--- asstes/js/button.js
+++ asstes/js/button.js
@@ -1,6 +1,11 @@
 
 var count = 0;
 var countEl = document.getElementById("count");
+
+document.getElementById("plus").onclick = function() {plus()};
+document.getElementById("minus").onclick = function() {minus()};
+
+
 function plus(){
     count++;
     countEl.value = count;

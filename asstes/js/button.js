6b7dd6dd59d82341a67417354c73a2158d3b09cc
Karl Rong <sjturjk@icloud.com>
2019-01-25 03:16:01 +0800

    Button click count

---
+++ asstes/js/button.js
@@ -0,0 +1,11 @@
+
+var count = 0;
+var countEl = document.getElementById("count");
+function plus(){
+    count++;
+    countEl.value = count;
+}
+function minus(){
+    count--;
+    countEl.value = count;
+}

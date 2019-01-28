a06cbd03ba8f2e5d61e37dc0b632c21634c63399
Karl Rong <sjturjk@icloud.com>
2019-01-28 11:55:14 +0800

    小问题

--- asstes/js/page2.js
+++ asstes/js/page2.js
@@ -7,27 +7,27 @@ for(var i = 0, len = avocados.length; i < len; ++i){
   var id = "#no" + (i + 1);
   switch(avocados[i]){
     case "mexico":
-      $(id).text("1. Avocado from Mesico");
+      $(id).text((i + 1) + ". Avocado from Mesico");
       x+=1.06;
       break;
     case "chile":
-      $(id).text("1. Avocado from Chile");
+      $(id).text((i + 1) + ". Avocado from Chile");
       x+=1.30;
       break;
     case "israel":
-      $(id).text("1. Avocado from Israel");
+      $(id).text((i + 1) + ". Avocado from Israel");
       x+=1.40;
       break;
     case "Columbia":
-      $(id).text("1. Avocado from Columbia");
+      $(id).text((i + 1) + ". Avocado from Columbia");
       x+=1.10;
       break;
     case "NewZealand":
-      $(id).text("1. Avocado from NewZealand");
+      $(id).text((i + 1) + ". Avocado from NewZealand");
       x+=1.50;
       break;
     case "fairtrade":
-      $(id).text("1. Avocado from Fairtrade");
+      $(id).text((i + 1) + ". Avocado from Fairtrade");
       x+=7;
       break;
     default:

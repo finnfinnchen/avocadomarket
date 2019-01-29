1dd635e5d6296e88e82ba6e38a9d95fbdbeff451
wu bin <532787286@qq.com>
2019-01-29 15:50:19 +0100

    icon

--- asstes/js/part2.js
+++ asstes/js/part2.js
@@ -1,6 +1,49 @@
-window.onload = function() {
-for(var i = 0; i < tr1.images.length; i++)
-   {
-     tr1.images[i].setAttribute('onclick','this.style.display="none
-   )
-}
+$("#tre1").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre2").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre3").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre4").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre5").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre6").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre7").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre8").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre9").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre10").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre11").click(function(){
+    $(this).addClass('hide');
+});
+
+$("#tre12").click(function(){
+    $(this).addClass('hide');
+});
+
+    

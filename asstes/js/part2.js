319416b60218e61e152e0249a221b233747725af
wu bin <532787286@qq.com>
2019-02-05 13:45:19 +0100

    test

--- asstes/js/part2.js
+++ asstes/js/part2.js
@@ -20,3 +20,41 @@ $("#importerbtn").click(function(event){
 $("#tre1").click(function(){
     $(this).addClass('hide');
 });
+
+$("#tre2").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre3").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre4").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre5").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre6").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre7").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre8").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre9").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre10").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre11").click(function(){
+    $(this).addClass('hide');
+});
+$("#tre12").click(function(){
+    $(this).addClass('hide');
+});
+
+
+
+
